b08699eb206abc832c69d52de0d350ffcf31e5bb
Tetiana Fomina <tetiana.fomina@brocoders.team>
2020-10-14 22:11:26 +0300

    Tasks 2_1 - 2_5

---
+++ 2_4 Task.js
@@ -0,0 +1,15 @@
+//Задача_1
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+// Задача_2
+let OurPlanetName;
+let CurrentUser;
+
+//Задача_3
+const BIRTHDAY = '18.04.1982'; // можем использовать заглавные буквы
+
+const AGE = someCode(BIRTHDAY); // для константы - age - не стоит использовать заглавные буквы, т.к. она вычисляется во время выполнения скрипта
